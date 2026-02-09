2b786f43285451261e867fb9f3023d8e8a74f20e
yogi-jitera <yogi.yudistira@jitera.com>
2026-02-09 17:25:47 +0900

    Clean up comments in reviewService.js
    Removed unnecessary comments from reviewService.js

--- services/reviewService.js
+++ services/reviewService.js
@@ -1,7 +1,5 @@
 const Review = require('../models/Review');
-//comment from user
-//add more comment
-//add more comment
+
 exports.getReviewsByBookId = async (bookId) => {
     return await Review.find({ book: bookId });
 };
