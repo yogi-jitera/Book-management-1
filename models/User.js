e4c8444b41558acb292cdfe7a96b28ae2b61e618
yogi-jitera <yogi.yudistira@jitera.com>
2026-02-11 13:41:50 +0900

    Create User model with schema definition

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('../config/db');
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
+    address: { type: String },
+    role: { type: String, enum: ['admin', 'staff'], default: 'admin' },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', userSchema);
